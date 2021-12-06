a3bdb67b8c3bed7a157cb5a275b43d540b639507
Dan James <dan@danarchy.me>
2021-12-06 11:14:59 -0800

    dd_best_bytesize.sh: add outfile argument, default /tmp/outfile

--- bash/dd_best_bytesize.sh
+++ bash/dd_best_bytesize.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+outfile=$([[ ${1} ]] && echo ${1} || echo '/tmp/outfile' )
 verbose=1
 best_bs=''
 best_speed=100.0
 
 echo 'Testing bytesizes: 1k 2k 4k 8k 16k 32k 64k 128k 256k 512k 1M 2M 4M 8M'
 for bs in 1k 2k 4k 8k 16k 32k 64k 128k 256k 512k 1M 2M 4M 8M ; do
-    speed=$(dd if=/dev/zero ibs=1M count=32 of=/tmp/test obs=${bs} 2>&1 | awk '/bytes/ {print$8}')
+    speed=$(dd if=/dev/zero ibs=1M count=32 of=${outfile} obs=${bs} 2>&1 | awk '/bytes/ {print$8}')
 
     if [[ $(bc <<< "${speed} <= ${best_speed}") -eq 1 ]]; then
         echo "Found new best speed: ${speed} | bs=${bs}"
@@ -17,3 +18,4 @@ for bs in 1k 2k 4k 8k 16k 32k 64k 128k 256k 512k 1M 2M 4M 8M ; do
 done
 
 echo -e "---\nRun dd with bs=${best_bs}"
+rm ${outfile}
